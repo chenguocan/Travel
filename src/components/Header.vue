458798224ae3b77256ba0eb08f4c2f0553c45408
chenguocan <2622870670@qq.com>
2020-09-17 18:10:11 +0800

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="city">
+        <span>城市</span>
+        <div class="iconfont">&#xe731;</div>
+      </div>
+    </div>
+    <div class="header-input">
+      <div class="iconfont">&#xe616;</div>
+      <input type="text" placeholder="搜索旅游地/酒店/景点" />
+    </div>
+    <div class="header-right">
+      <div class="iconfont foot"> &#xe600;</div>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/styles/variable.scss";
+@import "~@/assets/iconfont.css";
+.header {
+  height: 7vh;
+  display: flex;
+  justify-content: space-between;
+  font-size: 12px;
+  font-family: $font-hei;
+  align-items: center;
+  background: deepskyblue;
+  .header-left {
+    color:white;
+    .city{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding:0 15px 0 10px;
+    }
+  }
+  .header-input{
+    background: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    input {
+      font-size: 16px;
+      width: 100%;
+      height: 5vh;
+      padding: 5px;
+      background: white;
+    }
+  }
+  .header-right{
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    margin: 0 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgb(119,123,129);
+    .foot{
+      color:white;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import "./assets/styles/reset.scss"
+import "./assets/styles/border.scss"
+import fastClick from "fastclick"
+//移动端点击延迟问题
+fastClick.attach(document.body);
 
 Vue.config.productionTip = false
 
